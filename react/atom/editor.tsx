4352923fef5f84257b79d27a38732a45ac394667
hada0127 <tarucy@gmail.com>
2025-01-15 10:50:49 +0900

    fix: Form.Editor 업로드 헤더 추가

--- react/atom/editor.tsx
+++ react/atom/editor.tsx
@@ -31,6 +31,7 @@ interface Props {
   height?: string;
   onChange: (content: string) => void;
   imageUploadUrl?: string;
+  imageUploadHeader?: Record<string, string>;
   validator?: z.ZodType<unknown>;
 }
 
@@ -40,6 +41,7 @@ const SunEditor = ({
   height = '600px',
   onChange,
   imageUploadUrl,
+  imageUploadHeader,
   validator,
 }: Props) => {
   const [message, setMessage] = useState('');
@@ -64,83 +66,84 @@ const SunEditor = ({
       }
     }
   };
+  const toolbar = [
+    ['undo', 'redo'],
+    ['formatBlock', 'bold', 'underline', 'italic', 'strike'],
+    ['fontColor', 'hiliteColor'],
+    ['align', 'list', 'table'],
+    ['link', 'image', 'video'],
+    ['fullScreen', 'codeView'],
+  ];
+  const editorOptions = {
+    lang: lang.ko,
+    plugins: {
+      blockquote,
+      align,
+      font,
+      fontSize,
+      fontColor,
+      hiliteColor,
+      horizontalRule,
+      list,
+      table,
+      formatBlock,
+      lineHeight,
+      template,
+      paragraphStyle,
+      textStyle,
+      link,
+      image,
+      video,
+    },
+    width: width,
+    height: height,
+    buttonList: toolbar,
+    formats: [
+      'h1',
+      'h2',
+      'h3',
+      {
+        tag: 'p',
+        name: '본문',
+        command: 'replace',
+        class: '',
+      },
+      {
+        tag: 'div',
+        name: '인용구',
+        command: 'range',
+        class: '__se__format__range__quote',
+      },
+    ],
+    resizingBar: false,
+    imageUploadUrl,
+    imageUploadHeader,
+    icons: {
+      undo: '<i class="editor undo">실행취소</i>',
+      redo: '<i class="editor redo">다시실행</i>',
+      bold: '<i class="editor bold">굵게</i>',
+      underline: '<i class="editor underline">밑줄</i>',
+      italic: '<i class="editor italic">기울이기</i>',
+      strike: '<i class="editor strike">가운데선</i>',
+      font_color: '<i class="editor font">글자색</i>',
+      highlight_color: '<i class="editor highlighter">배경색</i>',
+      align_left: '<i class="editor align-left">왼쪽 정렬</i>',
+      align_center: '<i class="editor align-center">가운데 정렬</i>',
+      align_right: '<i class="editor align-right">우측 정렬</i>',
+      align_justify: '<i class="editor align-justify">양쪽 정렬</i>',
+
+      list_bullets: '<i class="editor list-ul">목록</i>',
+      list_number: '<i class="editor list-ol">숫자 목록</i>',
+      table: '<i class="editor table">테이블</i>',
+      link: '<i class="editor link">링크</i>',
+      image: '<i class="editor image">사진</i>',
+      video: '<i class="editor video">비디오</i>',
+      expansion: '<i class="editor expand">확장</i>',
+      code_view: '<i class="editor code">코드</i>',
+    },
+  };
 
   useEffect(() => {
-    const toolbar = [
-      ['undo', 'redo'],
-      ['formatBlock', 'bold', 'underline', 'italic', 'strike'],
-      ['fontColor', 'hiliteColor'],
-      ['align', 'list', 'table'],
-      ['link', 'image', 'video'],
-      ['fullScreen', 'codeView'],
-    ];
-    const editorOptions = {
-      lang: lang.ko,
-      plugins: {
-        blockquote,
-        align,
-        font,
-        fontSize,
-        fontColor,
-        hiliteColor,
-        horizontalRule,
-        list,
-        table,
-        formatBlock,
-        lineHeight,
-        template,
-        paragraphStyle,
-        textStyle,
-        link,
-        image,
-        video,
-      },
-      width: width,
-      height: height,
-      buttonList: toolbar,
-      formats: [
-        'h1',
-        'h2',
-        'h3',
-        {
-          tag: 'p',
-          name: '본문',
-          command: 'replace',
-          class: '',
-        },
-        {
-          tag: 'div',
-          name: '인용구',
-          command: 'range',
-          class: '__se__format__range__quote',
-        },
-      ],
-      resizingBar: false,
-      imageUploadUrl,
-      icons: {
-        undo: '<i class="editor undo">실행취소</i>',
-        redo: '<i class="editor redo">다시실행</i>',
-        bold: '<i class="editor bold">굵게</i>',
-        underline: '<i class="editor underline">밑줄</i>',
-        italic: '<i class="editor italic">기울이기</i>',
-        strike: '<i class="editor strike">가운데선</i>',
-        font_color: '<i class="editor font">글자색</i>',
-        highlight_color: '<i class="editor highlighter">배경색</i>',
-        align_left: '<i class="editor align-left">왼쪽 정렬</i>',
-        align_center: '<i class="editor align-center">가운데 정렬</i>',
-        align_right: '<i class="editor align-right">우측 정렬</i>',
-        align_justify: '<i class="editor align-justify">양쪽 정렬</i>',
-
-        list_bullets: '<i class="editor list-ul">목록</i>',
-        list_number: '<i class="editor list-ol">숫자 목록</i>',
-        table: '<i class="editor table">테이블</i>',
-        link: '<i class="editor link">링크</i>',
-        image: '<i class="editor image">사진</i>',
-        video: '<i class="editor video">비디오</i>',
-        expansion: '<i class="editor expand">확장</i>',
-        code_view: '<i class="editor code">코드</i>',
-      },
-    };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ref.current = suneditor.create(editorID, editorOptions as any);
 
@@ -157,9 +160,13 @@ const SunEditor = ({
 
   useEffect(() => {
     if (value) {
-      ref.current.setContents(value);
+      // 에디터 삭제 후 재생성
+      ref.current.destroy();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ref.current = suneditor.create(editorID, editorOptions as any);
     }
-  }, [value]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editorID, value]);
 
   return (
     <div className={`${styles.editor} ${statusClass}`}>
